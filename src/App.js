754c34eb31d88e302e43d71c4f068dc35ab6ce04
Bamdad Goshtasbi <bamdadgoshtasbi@gmail.com>
2023-05-21 18:28:46 -0700

    Started implementing Tic Tac Toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+         <h1>Hi Welcome to Tic_Tac_Toe</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+  
+      
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.exploratorium.edu/explore/puzzles/tictactoe#:~:text=Rules%20for%20Tic%2DTac%2DToe&text=Players%20take%20turns%20putting%20their,game%20ends%20in%20a%20tie."
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tic Tac Toe Rules
         </a>
       </header>
+    
     </div>
   );
 }
