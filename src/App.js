e7ac77aeb9c2447cc4a67c80fb56b3831aca6232
Bamdad Goshtasbi <bamdadgoshtasbi@gmail.com>
2023-05-21 18:55:56 -0700

    Added the basic rules

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-         <h1>Hi Welcome to Tic_Tac_Toe</h1>
-      <header className="App-header">
-  
-      
+      <h1>Hi Welcome to Tic_Tac_Toe</h1>
+      <p>
+        {/* Your content */}
+      </p>
+      <div className="bottom-link">
         <a
           className="App-link"
           href="https://www.exploratorium.edu/explore/puzzles/tictactoe#:~:text=Rules%20for%20Tic%2DTac%2DToe&text=Players%20take%20turns%20putting%20their,game%20ends%20in%20a%20tie."
           target="_blank"
           rel="noopener noreferrer"
         >
-          Tic Tac Toe Rules
+          Read more about Tic Tac Toe Rules
         </a>
-      </header>
-    
+      </div>
     </div>
   );
 }
