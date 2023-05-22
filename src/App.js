90b93fa403a07b9766415c179ca5851c0b3c3dff
Bamdad Goshtasbi <bamdadgoshtasbi@gmail.com>
2023-05-21 20:06:32 -0700

    Changing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <div>
+
+
+
+
     <div className="App">
-      <h1>Hi Welcome to Tic_Tac_Toe</h1>
-      <p>
-        {/* Your content */}
-      </p>
-      <div className="bottom-link">
+         <h1>Hi Welcome to Tic_Tac_Toe</h1>
+      <header className="App-header">
         <a
           className="App-link"
           href="https://www.exploratorium.edu/explore/puzzles/tictactoe#:~:text=Rules%20for%20Tic%2DTac%2DToe&text=Players%20take%20turns%20putting%20their,game%20ends%20in%20a%20tie."
@@ -18,7 +19,10 @@ function App() {
         >
           Read more about Tic Tac Toe Rules
         </a>
-      </div>
+      </header>
+    </div>
+
+
     </div>
   );
 }
